6da208cfef485c4b559aaa08adb86d989c333bc1
James Harper <james.harper@bendigoit.com.au>
2007-11-11 15:16:58 +1100

    Added a new PCI driver that can hide the Xen provided PCI devices

---
+++ xenhide/xenhide.c
@@ -0,0 +1,310 @@
+/*
+PV Drivers for Windows Xen HVM Domains
+Copyright (C) 2007 James Harper
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+#include "xenhide.h"
+#include <stdlib.h>
+
+DRIVER_INITIALIZE DriverEntry;
+static NTSTATUS
+XenHide_AddDevice(WDFDRIVER Driver, PWDFDEVICE_INIT DeviceInit);
+static VOID
+XenHide_IoInternalDeviceControl(WDFQUEUE Queue, WDFREQUEST Request, size_t OutputBufferLength, size_t InputBufferLength, ULONG IoControlCode);
+
+#ifdef ALLOC_PRAGMA
+#pragma alloc_text (INIT, DriverEntry)
+#pragma alloc_text (PAGE, XenHide_AddDevice)
+#endif
+
+static BOOLEAN AutoEnumerate;
+
+static WDFDEVICE Device;
+
+NTSTATUS
+DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath)
+{
+  WDF_DRIVER_CONFIG config;
+  NTSTATUS status;
+  UNICODE_STRING RegKeyName;
+  UNICODE_STRING RegValueName;
+  HANDLE RegHandle;
+  OBJECT_ATTRIBUTES RegObjectAttributes;
+  char Buf[200];
+  ULONG BufLen = 200;
+  PKEY_VALUE_PARTIAL_INFORMATION KeyPartialValue;
+  int State = 0;
+  int StartPos = 0;
+  WCHAR *SystemStartOptions;
+  size_t SystemStartOptionsLen;
+  size_t i;
+
+  KdPrint((__DRIVER_NAME " --> DriverEntry\n"));
+
+  RtlInitUnicodeString(&RegKeyName, L"\\Registry\\Machine\\System\\CurrentControlSet\\Control");
+  InitializeObjectAttributes(&RegObjectAttributes, &RegKeyName, OBJ_CASE_INSENSITIVE, NULL, NULL);
+  status = ZwOpenKey(&RegHandle, KEY_READ, &RegObjectAttributes);
+  if(!NT_SUCCESS(status))
+  {
+    KdPrint((__DRIVER_NAME "     ZwOpenKey returned %08x\n", status));
+  }
+
+  RtlInitUnicodeString(&RegValueName, L"SystemStartOptions");
+  status = ZwQueryValueKey(RegHandle, &RegValueName, KeyValuePartialInformation, Buf, BufLen, &BufLen);
+  if(!NT_SUCCESS(status))
+  {
+    KdPrint((__DRIVER_NAME "     ZwQueryKeyValue returned %08x\n", status));
+  }
+  KdPrint((__DRIVER_NAME "     BufLen = %d\n", BufLen));
+  KeyPartialValue = (PKEY_VALUE_PARTIAL_INFORMATION)Buf;
+  KdPrint((__DRIVER_NAME "     Buf = %ws\n", KeyPartialValue->Data));
+  SystemStartOptions = (WCHAR *)KeyPartialValue->Data;
+
+  AutoEnumerate = FALSE;
+
+  RtlStringCbLengthW(SystemStartOptions, KeyPartialValue->DataLength, &SystemStartOptionsLen);
+
+  for (i = 0; i <= SystemStartOptionsLen/2; i++)
+  {
+    //KdPrint((__DRIVER_NAME "     pos = %d, state = %d, char = '%wc' (%d)\n", i, State, SystemStartOptions[i], SystemStartOptions[i]));
+    
+    switch (State)
+    {
+    case 0:
+      if (SystemStartOptions[i] == L'G')
+      {
+        StartPos = i;
+        State = 2;
+      } else if (SystemStartOptions[i] != L' ')
+      {
+        State = 1;
+      }
+      break;
+    case 1:
+      if (SystemStartOptions[i] == L' ')
+        State = 0;
+      break;
+    case 2:
+      if (SystemStartOptions[i] == L'P')
+        State = 3;
+      else
+        State = 0;
+      break;
+    case 3:
+      if (SystemStartOptions[i] == L'L')
+        State = 4;
+      else
+        State = 0;
+      break;
+    case 4:
+      if (SystemStartOptions[i] == L'P')
+        State = 5;
+      else
+        State = 0;
+      break;
+    case 5:
+      if (SystemStartOptions[i] == L'V')
+        State = 6;
+      else
+        State = 0;
+      break;
+    case 6:
+      if (SystemStartOptions[i] == L' ' || SystemStartOptions[i] == 0)
+        AutoEnumerate = TRUE;
+      State = 0;
+      break;
+    }
+  }
+
+  KdPrint((__DRIVER_NAME "     AutoEnumerate = %d\n", AutoEnumerate));
+
+
+  WDF_DRIVER_CONFIG_INIT(&config, XenHide_AddDevice);
+  status = WdfDriverCreate(
+                      DriverObject,
+                      RegistryPath,
+                      WDF_NO_OBJECT_ATTRIBUTES,
+                      &config,
+                      WDF_NO_HANDLE);
+  if(!NT_SUCCESS(status))
+  {
+    //KdPrint((__DRIVER_NAME " WdfDriverCreate failed with status 0x%08x\n", status));
+  }
+
+  KdPrint((__DRIVER_NAME " <-- DriverEntry\n"));
+
+  return status;
+}
+
+static NTSTATUS
+XenHide_PreprocessWdmIrpPNP(WDFDEVICE Device, PIRP Irp);
+
+static NTSTATUS
+XenHide_AddDevice(
+    IN WDFDRIVER Driver,
+    IN PWDFDEVICE_INIT DeviceInit
+    )
+{
+  NTSTATUS status;
+  WDF_OBJECT_ATTRIBUTES attributes;
+  UCHAR MinorFunctions[1] = { IRP_MN_QUERY_DEVICE_RELATIONS };
+
+  UNREFERENCED_PARAMETER(Driver);
+
+  KdPrint((__DRIVER_NAME " --> DeviceAdd\n"));
+
+  WdfFdoInitSetFilter(DeviceInit);
+
+  WDF_OBJECT_ATTRIBUTES_INIT(&attributes);
+
+  WdfDeviceInitAssignWdmIrpPreprocessCallback(DeviceInit, XenHide_PreprocessWdmIrpPNP, IRP_MJ_PNP, MinorFunctions, 1);
+
+  status = WdfDeviceCreate(&DeviceInit, &attributes, &Device);  
+  if(!NT_SUCCESS(status))
+  {
+    KdPrint((__DRIVER_NAME "     WdfDeviceCreate failed with status 0x%08x\n", status));
+    return status;
+  }
+
+  KdPrint((__DRIVER_NAME " <-- DeviceAdd\n"));
+
+  return status;
+}
+
+static int
+XenHide_StringMatches(PWCHAR String1, PWCHAR String2)
+{
+  for(;*String1 != 0 && *String2 != 0 && *String1 == *String2; String1++, String2++);
+  return ((*String1 == 0 && *String2 == 0) || (*String1 == 0 && *String2 == L'\n') || (*String1 == L'\n' && *String2 == 0));
+}
+
+static NTSTATUS
+XenHide_IoCompletion(PDEVICE_OBJECT DeviceObject, PIRP Irp, PVOID Context)
+{
+  ULONG i;
+  PDEVICE_RELATIONS Relations;
+  WCHAR Buffer[1000];
+  PWCHAR Ptr;
+  ULONG Length;
+  size_t StrLen;
+  int Match;
+  int Offset = 0;
+
+  UNREFERENCED_PARAMETER(DeviceObject);
+  UNREFERENCED_PARAMETER(Context);
+
+  KdPrint((__DRIVER_NAME " --> IoCompletion\n"));
+
+  Relations = (PDEVICE_RELATIONS)Irp->IoStatus.Information;
+
+  for (i = 0; i < Relations->Count; i++)
+  {
+    Length = 1000;
+    IoGetDeviceProperty(Relations->Objects[i], DevicePropertyDeviceDescription, Length, Buffer, &Length);
+    KdPrint((__DRIVER_NAME "     %3d - %ws\n", i, Buffer));
+    Length = 1000;
+    IoGetDeviceProperty(Relations->Objects[i], DevicePropertyHardwareID, Length, Buffer, &Length);
+    Match = 0;
+    StrLen = 0;
+    for (Ptr = Buffer; *Ptr != 0; Ptr += StrLen + 1)
+    {
+      KdPrint((__DRIVER_NAME "         - %ws\n", Ptr));
+      if (XenHide_StringMatches(Ptr, L"PCI\\VEN_8086&DEV_7010&SUBSYS_00015853")) {
+        Match = 1;
+        break;
+      }
+      RtlStringCchLengthW(Ptr, Length, &StrLen);
+    }
+    if (Match)
+    {
+      KdPrint((__DRIVER_NAME "           (Match)\n"));
+      Offset++;
+    }
+    if (Offset != 0)
+      Relations->Objects[i] = Relations->Objects[i + Offset];
+  }
+  Relations->Count -= Offset;
+  
+  KdPrint((__DRIVER_NAME " <-- IoCompletion\n"));
+
+  return Irp->IoStatus.Status;
+}
+
+
+
+static NTSTATUS
+XenHide_PreprocessWdmIrpPNP(WDFDEVICE Device, PIRP Irp)
+{
+  NTSTATUS Status = STATUS_SUCCESS;
+  PIO_STACK_LOCATION Stack;
+
+  KdPrint((__DRIVER_NAME " --> WdmIrpPreprocessPNP\n"));
+
+  Stack = IoGetCurrentIrpStackLocation(Irp);
+
+  switch (Stack->MinorFunction) {
+  case IRP_MN_QUERY_DEVICE_RELATIONS:
+    KdPrint((__DRIVER_NAME "     IRP_MN_QUERY_DEVICE_RELATIONS Device = %08x Irp = %08x, Stack = %08x\n", Device, Irp, Stack));
+    switch (Stack->Parameters.QueryDeviceRelations.Type)
+    {
+    case BusRelations:
+      KdPrint((__DRIVER_NAME "       BusRelations\n"));
+      if (AutoEnumerate)
+      {
+        IoCopyCurrentIrpStackLocationToNext(Irp);
+        IoSetCompletionRoutine(Irp, XenHide_IoCompletion, NULL, TRUE, TRUE, TRUE);
+        Status = WdfDeviceWdmDispatchPreprocessedIrp(Device, Irp);
+      }
+      else
+      {
+        IoSkipCurrentIrpStackLocation(Irp);
+        Status = WdfDeviceWdmDispatchPreprocessedIrp(Device, Irp);
+      }
+      break;  
+    case EjectionRelations: 
+      IoSkipCurrentIrpStackLocation(Irp);
+      Status = WdfDeviceWdmDispatchPreprocessedIrp(Device, Irp);
+      KdPrint((__DRIVER_NAME "       EjectionRelations\n"));
+      break;  
+    case RemovalRelations:
+      IoSkipCurrentIrpStackLocation(Irp);
+      Status = WdfDeviceWdmDispatchPreprocessedIrp(Device, Irp);
+      KdPrint((__DRIVER_NAME "       RemovalRelations\n"));
+      break;  
+    case TargetDeviceRelation:
+      IoSkipCurrentIrpStackLocation(Irp);
+      Status = WdfDeviceWdmDispatchPreprocessedIrp(Device, Irp);
+      KdPrint((__DRIVER_NAME "       TargetDeviceRelation\n"));
+      break;  
+    default:
+      IoSkipCurrentIrpStackLocation(Irp);
+      Status = WdfDeviceWdmDispatchPreprocessedIrp(Device, Irp);
+      KdPrint((__DRIVER_NAME "     Unknown Type %d\n", Stack->Parameters.QueryDeviceRelations.Type));
+      break;  
+    }
+    break;
+  default:
+    IoSkipCurrentIrpStackLocation(Irp);
+    Status = WdfDeviceWdmDispatchPreprocessedIrp(Device, Irp);
+    KdPrint((__DRIVER_NAME "     Unknown Minor %d\n", Stack->MinorFunction));
+    break;
+  }
+
+  KdPrint((__DRIVER_NAME " <-- WdmIrpPreprocessPNP (returning with status %08x\n", Status));
+
+  return Status;
+}
---
+++ xenhide/xenhide.h
@@ -0,0 +1,105 @@
+/*
+PV Drivers for Windows Xen HVM Domains
+Copyright (C) 2007 James Harper
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+#if !defined(_XENHIDE_H_)
+#define _XENHIDE_H_
+
+#define __attribute__(arg) /* empty */
+#define EISCONN 127
+
+#include <ntddk.h>
+#include <wdm.h>
+#include <wdf.h>
+#include <initguid.h>
+#include <wdmguid.h>
+#include <errno.h>
+
+#define NTSTRSAFE_LIB
+#include <ntstrsafe.h>
+
+#include <xen_windows.h>
+
+#include <memory.h>
+#include <grant_table.h>
+#include <event_channel.h>
+#include <hvm/params.h>
+#include <hvm/hvm_op.h>
+
+//{C828ABE9-14CA-4445-BAA6-82C2376C6518}
+//DEFINE_GUID( GUID_XENPCI_DEVCLASS, 0xC828ABE9, 0x14CA, 0x4445, 0xBA, 0xA6, 0x82, 0xC2, 0x37, 0x6C, 0x65, 0x18);
+
+#define __DRIVER_NAME "XenHide"
+#define XENHIDE_POOL_TAG (ULONG) 'XenH'
+//#define XENHIDE_FDO_INSTANCE_SIGNATURE (ULONG) 'XENP'
+
+#define NR_RESERVED_ENTRIES 8
+#define NR_GRANT_FRAMES 4
+#define NR_GRANT_ENTRIES (NR_GRANT_FRAMES * PAGE_SIZE / sizeof(grant_entry_t))
+
+#define ARRAY_SIZE(x) (sizeof(x) / sizeof((x)[0]))
+
+extern char *hypercall_stubs;
+
+typedef struct _XENHIDE_IDENTIFICATION_DESCRIPTION
+{
+  WDF_CHILD_IDENTIFICATION_DESCRIPTION_HEADER Header;
+  UNICODE_STRING DeviceType;
+//  ULONG DeviceIndex;
+  char Path[128];
+} XENHIDE_IDENTIFICATION_DESCRIPTION, *PXENHIDE_IDENTIFICATION_DESCRIPTION;
+
+
+typedef struct {
+  WDFQUEUE          IoDefaultQueue;
+
+  // Resources
+  //WDFINTERRUPT      Interrupt;
+  //PULONG            PhysAddress;
+
+  //ULONG platform_mmio_addr;
+  //ULONG platform_mmio_len;
+  //ULONG platform_mmio_alloc;
+
+  //ULONG shared_info_frame;
+  //char *hypercall_stubs;
+
+  //PULONG            IoBaseAddress;
+  //ULONG             IoRange;
+
+  // Grant Table stuff
+
+  //grant_entry_t *gnttab_table;
+  //grant_ref_t gnttab_list[NR_GRANT_ENTRIES];
+
+} XENHIDE_DEVICE_DATA, *PXENHIDE_DEVICE_DATA;
+
+WDF_DECLARE_CONTEXT_TYPE_WITH_NAME(XENHIDE_DEVICE_DATA, GetDeviceData);
+
+typedef unsigned long xenbus_transaction_t;
+typedef uint32_t XENSTORE_RING_IDX;
+
+#define XBT_NIL ((xenbus_transaction_t)0)
+
+#include <evtchn_public.h>
+#include <xenbus_public.h>
+#include <xen_public.h>
+#include <gnttbl_public.h>
+
+
+#endif
