efeccd2197e6900c365f1e1cd34783c0abf71783
Andy Grover <andy@groveronline.com>
2007-11-29 12:35:19 -0800

    use equivalent types for typedefs (actually the old ones are equivalent too, this is purely a readability change)

--- common/include/xen_windows.h
+++ common/include/xen_windows.h
@@ -6,13 +6,13 @@
 
 #define __XEN_INTERFACE_VERSION__ 0x00030205
 #define __i386__
-typedef signed char int8_t;
-typedef unsigned char uint8_t;
-typedef SHORT int16_t;
-typedef USHORT uint16_t;
-typedef LONG int32_t;
-typedef ULONG uint32_t;
-typedef ULONGLONG uint64_t;
+typedef INT8 int8_t;
+typedef UINT8 uint8_t;
+typedef INT16 int16_t;
+typedef UINT16 uint16_t;
+typedef INT32 int32_t;
+typedef UINT32 uint32_t;
+typedef UINT64 uint64_t;
 typedef unsigned long pgentry_t;
 
 #include <xen.h>
