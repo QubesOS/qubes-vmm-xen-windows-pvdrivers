53516a420459439cbe576ac2d9f6da36ecfc7e5c
James Harper <james.harper@bendigoit.com.au>
2008-01-11 23:47:45 +1100

    fix bugs in shutdown - thanks to Nico Prenzel

--- ShutdownMon/Program.cs
+++ ShutdownMon/Program.cs
@@ -15,6 +15,66 @@ namespace ShutdownMon
 {
     class Program : ServiceBase
     {
+        /* 
+        * Shutdown definition see here: http://msdn2.microsoft.com/en-us/library/aa376868.aspx
+        */
+
+        //public enum ExitWindows : uint
+        internal enum ExitWindows : uint
+        {
+            // ONE of the following five:
+            LogOff = 0x00,
+            ShutDown = 0x01,
+            Reboot = 0x02,
+            PowerOff = 0x08,
+            RestartApps = 0x40,
+            // plus AT MOST ONE of the following two:
+            Force = 0x04,
+            ForceIfHung = 0x10,
+        }
+
+        internal enum ShutdownReason : uint
+        {
+            MajorApplication = 0x00040000,
+            MajorHardware = 0x00010000,
+            MajorLegacyApi = 0x00070000,
+            MajorOperatingSystem = 0x00020000,
+            MajorOther = 0x00000000,
+            MajorPower = 0x00060000,
+            MajorSoftware = 0x00030000,
+            MajorSystem = 0x00050000,
+
+            MinorBlueScreen = 0x0000000F,
+            MinorCordUnplugged = 0x0000000b,
+            MinorDisk = 0x00000007,
+            MinorEnvironment = 0x0000000c,
+            MinorHardwareDriver = 0x0000000d,
+            MinorHotfix = 0x00000011,
+            MinorHung = 0x00000005,
+            MinorInstallation = 0x00000002,
+            MinorMaintenance = 0x00000001,
+            MinorMMC = 0x00000019,
+            MinorNetworkConnectivity = 0x00000014,
+            MinorNetworkCard = 0x00000009,
+            MinorOther = 0x00000000,
+            MinorOtherDriver = 0x0000000e,
+            MinorPowerSupply = 0x0000000a,
+            MinorProcessor = 0x00000008,
+            MinorReconfig = 0x00000004,
+            MinorSecurity = 0x00000013,
+            MinorSecurityFix = 0x00000012,
+            MinorSecurityFixUninstall = 0x00000018,
+            MinorServicePack = 0x00000010,
+            MinorServicePackUninstall = 0x00000016,
+            MinorTermSrv = 0x00000020,
+            MinorUnstable = 0x00000006,
+            MinorUpgrade = 0x00000003,
+            MinorWMI = 0x00000015,
+
+            FlagUserDefined = 0x40000000,
+            FlagPlanned = 0x80000000
+        }
+
         const UInt32 READ_CONTROL = 0x00020000;
         const UInt32 STANDARD_RIGHTS_READ = READ_CONTROL;
         const UInt32 FILE_READ_DATA = 0x0001;
@@ -56,20 +116,16 @@ namespace ShutdownMon
         internal static extern bool AdjustTokenPrivileges(IntPtr htok, bool disall, ref TokPriv1Luid newst, int len, IntPtr prev, IntPtr relen);
 
         [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
-        internal static extern bool ExitWindowsEx(int flg, int rea);
-
+        
+        internal static extern bool ExitWindowsEx(ExitWindows uFlags, ShutdownReason dwReason);
+        
         internal const int SE_PRIVILEGE_ENABLED = 0x00000002;
         internal const int TOKEN_QUERY = 0x00000008;
         internal const int TOKEN_ADJUST_PRIVILEGES = 0x00000020;
         internal const string SE_SHUTDOWN_NAME = "SeShutdownPrivilege";
-        internal const int EWX_LOGOFF = 0x00000000;
-        internal const int EWX_SHUTDOWN = 0x00000001;
-        internal const int EWX_REBOOT = 0x00000002;
-        internal const int EWX_FORCE = 0x00000004;
-        internal const int EWX_POWEROFF = 0x00000008;
-        internal const int EWX_FORCEIFHUNG = 0x00000010;
 
-        private static void DoExitWin(int flg)
+
+        private static void DoExitWin(ExitWindows flg)
         {
             bool ok;
             TokPriv1Luid tp;
@@ -82,7 +138,7 @@ namespace ShutdownMon
             ok = LookupPrivilegeValue(null, SE_SHUTDOWN_NAME, ref tp.Luid);
             ok = AdjustTokenPrivileges(htok, false, ref tp, 0, IntPtr.Zero,
             IntPtr.Zero);
-            ok = ExitWindowsEx(flg, 0);
+            ok = ExitWindowsEx(flg, ShutdownReason.MajorOther & ShutdownReason.MinorOther);
         }
 
         const string MyServiceName = "XenShutdownMon";
@@ -241,12 +297,14 @@ namespace ShutdownMon
                     case "":
                         break;
                     case "reboot":
-                        DoExitWin(EWX_REBOOT | EWX_FORCE);
+                        //DoExitWin(EWX_REBOOT | EWX_FORCE);
+                        DoExitWin(ExitWindows.Reboot | ExitWindows.ForceIfHung);
                         break;
                     case "poweroff":
                     case "halt":
                     default:
-                        DoExitWin(EWX_POWEROFF | EWX_FORCE);
+                        //DoExitWin(EWX_POWEROFF | EWX_FORCE);
+                        DoExitWin(ExitWindows.PowerOff | ExitWindows.ForceIfHung);
                         break;
                 }
             }
