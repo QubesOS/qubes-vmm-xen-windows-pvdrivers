152c8d147869be4b2dd5df362bb210f71942b420
James Harper <james.harper@bendigoit.com.au>
2008-03-30 00:03:49 +1100

    fixed version detection in install.bat - sometimes version is spelled with a capital and sometimes not

--- target/install.bat
+++ target/install.bat
@@ -1,15 +1,15 @@
 @ECHO OFF
 
-ver | find "Version 5.00." > nul
+ver | find /I "Version 5.00." > nul
 if %ERRORLEVEL% == 0 goto ver_2k
 
-ver | find "Version 5.1." > nul
+ver | find /I "Version 5.1." > nul
 if %ERRORLEVEL% == 0 goto ver_xp
 
-ver | find "Version 5.2." > nul
+ver | find /I "Version 5.2." > nul
 if %ERRORLEVEL% == 0 goto ver_2k3
 
-ver | find "Version 6.0." > nul
+ver | find /I "Version 6.0." > nul
 if %ERRORLEVEL% == 0 goto ver_2k8
 
 echo No automatic install available or machine not supported.
