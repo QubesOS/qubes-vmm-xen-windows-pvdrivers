4fca505ed13b373ac4bbb7585148e5432f0f506f
Andy Grover <andy.grover@oracle.com>
2008-07-01 15:22:55 -0700

    remove unused stuff from mingw assembly file; implement getting stub_addr from device extension (gcc extended asm ftw!)

--- xenpci/hypercall_x86_mingw.h
+++ xenpci/hypercall_x86_mingw.h
@@ -23,64 +23,37 @@
 #define __STR(x) #x
 #define STR(x) __STR(x)
 
-#define HYPERCALL_STR(name)					\
-	"mov $stub_addr,%%eax; "			    \
-	"add $("STR(__HYPERVISOR_##name)" * 32),%%eax; "	\
+#if 0
+#define HYPERCALL_STR(name)                                     \
+	"mov $stub_addr,%%eax; "                                      \
+	"add $("STR(__HYPERVISOR_##name)" * 32),%%eax; "              \
+	"call *%%eax"
+#endif
+
+#define HYPERCALL_STR(name)                                     \
+	"mov %3,%%eax; "                                      \
+	"add $("STR(__HYPERVISOR_##name)" * 32),%%eax; "              \
 	"call *%%eax"
 
 #define _hypercall2(type, name, a1, a2)                         \
 ({                                                              \
-        long __res, __ign1, __ign2;                             \
-        char *stub_addr;                                        \
-        stub_addr = xpdd->hypercall_stubs;                      \
-        asm volatile (                                          \
-                HYPERCALL_STR(name)                             \
-                : "=a" (__res), "=b" (__ign1), "=c" (__ign2)    \
-                : "1" ((long)(a1)), "2" ((long)(a2))            \
-                : "memory" );                                   \
-        (type)__res;                                            \
+  long __res, __ign1, __ign2;                                   \
+  asm volatile (                                                \
+    HYPERCALL_STR(name)                                         \
+    : "=a" (__res), "=b" (__ign1), "=c" (__ign2)                \
+    : "1" ((long)(a1)), "2" ((long)(a2)), "r" (xpdd->hypercall_stubs) \
+    : "memory" );                                               \
+  (type)__res;                                                  \
 })
-
-#define _hypercall3(type, name, a1, a2, a3)			\
-({								\
-	long __res, __ign1, __ign2, __ign3;			\
-	asm volatile (						\
-		HYPERCALL_STR(name)				\
-		: "=a" (__res), "=b" (__ign1), "=c" (__ign2), 	\
-		"=d" (__ign3)					\
-		: "1" ((long)(a1)), "2" ((long)(a2)),		\
-		"3" ((long)(a3))				\
-		: "memory" );					\
-	(type)__res;						\
-})
-
-#define _hypercall4(type, name, a1, a2, a3, a4)			\
-({								\
-	long __res, __ign1, __ign2, __ign3, __ign4;		\
-	asm volatile (						\
-		HYPERCALL_STR(name)				\
-		: "=a" (__res), "=b" (__ign1), "=c" (__ign2),	\
-		"=d" (__ign3), "=S" (__ign4)			\
-		: "1" ((long)(a1)), "2" ((long)(a2)),		\
-		"3" ((long)(a3)), "4" ((long)(a4))		\
-		: "memory" );					\
-	(type)__res;						\
-})
-
-static inline void cpuid(int op, unsigned int *eax, unsigned int *ebx,
-                         unsigned int *ecx, unsigned int *edx)
-{
-  __asm__("cpuid"
-          : "=a" (*eax),
-            "=b" (*ebx),
-            "=c" (*ecx),
-            "=d" (*edx)
-          : "0" (op));
-}
 
 static __inline void __cpuid(uint32_t output[4], uint32_t op)
 {
-  cpuid(op, &output[0], &output[1], &output[2], &output[3]);
+  __asm__("cpuid"
+          : "=a" (output[0]),
+            "=b" (output[1]),
+            "=c" (output[2]),
+            "=d" (output[3])
+          : "0" (op));
 }
 
 static __inline void __writemsr(uint32_t msr, uint64_t value)
@@ -104,30 +77,6 @@ static __inline int
 HYPERVISOR_sched_op(PXENPCI_DEVICE_DATA xpdd, int cmd, void *arg)
 {
   return _hypercall2(int, sched_op, cmd, arg);
-}
-
-static __inline int
-HYPERVISOR_xen_version(PXENPCI_DEVICE_DATA xpdd, int cmd, void *arg)
-{
-  return _hypercall2(int, xen_version, cmd, arg);
-}
-
-static __inline int
-HYPERVISOR_grant_table_op(PXENPCI_DEVICE_DATA xpdd, int cmd, void *uop, unsigned int count)
-{
-	return _hypercall3(int, grant_table_op, cmd, uop, count);
-}
-
-static __inline int
-HYPERVISOR_mmu_update(PXENPCI_DEVICE_DATA xpdd, mmu_update_t *req, int count, int *success_count, domid_t domid)
-{
-	return _hypercall4(int, mmu_update, req, count, success_count, domid);
-}
-
-static __inline int
-HYPERVISOR_console_io(PXENPCI_DEVICE_DATA xpdd, int cmd, int count, char *string)
-{
-	return _hypercall3(int, console_io, cmd, count, string);
 }
 
 static __inline int
