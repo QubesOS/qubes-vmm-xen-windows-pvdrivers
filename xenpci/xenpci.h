87956b26d91e19a4de78ea4b4381d3a800d88365
Andy Grover <andy.grover@oracle.com>
2008-07-09 00:10:57 -0700

    modify xenpci mingw build for new file locations

--- xenpci/xenpci.h
+++ xenpci/xenpci.h
@@ -26,7 +26,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include <ntddk.h>
 
 #ifdef __MINGW32__
-#include "mingw_extras.h"
+#include "../mingw/mingw_extras.h"
 #else
 #define DDKAPI
 #include <wdm.h>
