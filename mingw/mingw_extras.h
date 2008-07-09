9029d97fcb558f2ad9b67ec518f5d9aeb179b42f
Andy Grover <andy.grover@oracle.com>
2008-07-09 12:32:12 -0700

    move bit ops to mingw_extras since now xenvbd and evtchn.c are using them. no longer inline but shouldn't matter, eh?

--- mingw/mingw_extras.h
+++ mingw/mingw_extras.h
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+NTSTATUS bit_scan_forward(unsigned long *index, unsigned long mask);
+int synch_set_bit(int nr, volatile long * addr);
+int synch_clear_bit(int nr, volatile long * addr);
+
 /* windows wchar 2 bytes, Linux's is 4! */
 typedef unsigned short win_wchar_t;
 
