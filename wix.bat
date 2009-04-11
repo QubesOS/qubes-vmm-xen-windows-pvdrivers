1af2d76f5d2306f0a49f7c4fc5852373f6570256
James Harper <james.harper@bendigoit.com.au>
2009-04-11 10:29:54 +1000

    name the msi file with the version number too

--- wix.bat
+++ wix.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
 IF "%_BUILDARCH%"=="x86" (SET DIFXLIB=%WIX%bin\difxapp_x86.wixlib) ELSE (SET DIFXLIB=%WIX%bin\difxapp_x64.wixlib)
 "%WIX%\bin\candle" installer.wxs -ext "%WIX%\bin\WixUIExtension.dll" -ext "%WIX%\bin\WixDifxAppExtension.dll" -ext "%WIX%\bin\WixIIsExtension.dll"
-"%WIX%\bin\light.exe" -o gplpv_%BUILD_ALT_DIR%.msi installer.wixobj "%DIFXLIB%" -ext "%WIX%\bin\WixUIExtension.dll" -ext "%WIX%\bin\WixDifxAppExtension.dll" -ext "%WIX%\bin\WixIIsExtension.dll"
+"%WIX%\bin\light.exe" -o gplpv_%BUILD_ALT_DIR%_%GPLPV_VERSION%.msi installer.wixobj "%DIFXLIB%" -ext "%WIX%\bin\WixUIExtension.dll" -ext "%WIX%\bin\WixDifxAppExtension.dll" -ext "%WIX%\bin\WixIIsExtension.dll"
