1a7053651f62366fbdc352ed431512f5519d0c86
James Harper <james.harper@bendigoit.com.au>
2009-07-15 19:54:03 +1000

    Tag hg on build when the TAG_HG file is present

--- makedist.bat
+++ makedist.bat
@@ -37,3 +37,11 @@ cmd /C "%DDK_PATH%\bin\setenv.bat %DDK_PATH%\ chk x64 WNET && CD /D "%PV_DIR%" &
 cmd /C "%DDK_PATH%\bin\setenv.bat %DDK_PATH%\ chk WLH && CD /D "%PV_DIR%" && build -cZg && call sign.bat && call wix.bat"
 
 cmd /C "%DDK_PATH%\bin\setenv.bat %DDK_PATH%\ chk x64 WLH && CD /D "%PV_DIR%" && build -cZg && call sign.bat && call wix.bat"
+
+IF NOT EXIST TAG_HG GOTO DONT_TAG
+IF EXIST BUILD*.WRN GOTO DONT_TAG
+IF EXIST BUILD*.ERR GOTO DONT_TAG
+
+hg tag %GPLPV_VERSION%
+
+:DONT_TAG
