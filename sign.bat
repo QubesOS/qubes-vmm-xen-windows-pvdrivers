9b03f36e43393b8f7b08e88969a0328e74915f73
Amir Szekely <amir@neocleus.com>
2009-08-18 17:37:27 +0300

    avoid 7b bsod by better defining installation and driver loading order so xenpci won't hide the boot disk before xenvbd is properly installed  - only hide devices once the pv driver to load their counterpart device is installed (move hide_device list creation from xenpci.inf to xenscsi.inf, xenvbd.inf, xennet.inf)  - remove default hiding list (in case no drivers were installed yet)  - force a reboot after xenvbd installation to avoid having both it and the drivers it should replace running at the same time causing disk corruption  - hide more xenvbd conflicting devices

--- sign.bat
+++ sign.bat
@@ -11,7 +11,8 @@ ECHO _BUILDARCH=%_BUILDARCH%
 ECHO BUILDDIR=%BUILDDIR%
 ECHO SIGN_OS=%SIGN_OS%
 
-xcopy /D xenhide\%BUILDDIR%\xenhide.sys xenpci\%BUILDDIR%
+xcopy /D coinst\%BUILDDIR%\coinst.dll xenvbd\%BUILDDIR%
+move xenvbd\%BUILDDIR%\coinst.dll xenvbd\%BUILDDIR%\xencoinst.dll
 xcopy /D %BASEDIR%\redist\wdf\%_BUILDARCH%\WdfCoInstaller01007.dll xenpci\%BUILDDIR%
 
 IF NOT EXIST SIGN_CONFIG.BAT GOTO DONT_SIGN
@@ -23,7 +24,7 @@ CALL SIGN_CONFIG.BAT
 %DDK_PATH%\bin\selfsign\signtool sign /v /n %CERT_NAME% /t http://timestamp.verisign.com/scripts/timestamp.dll xennet\%BUILDDIR%\xennet.sys xennet\%BUILDDIR%\xennet.cat
 
 %DDK_PATH%\bin\selfsign\inf2cat /driver:xenvbd\%BUILDDIR% /os:%SIGN_OS%
-%DDK_PATH%\bin\selfsign\signtool sign /v /n %CERT_NAME% /t http://timestamp.verisign.com/scripts/timestamp.dll xenvbd\%BUILDDIR%\xenvbd.sys xenvbd\%BUILDDIR%\xenvbd.cat
+%DDK_PATH%\bin\selfsign\signtool sign /v /n %CERT_NAME% /t http://timestamp.verisign.com/scripts/timestamp.dll xenvbd\%BUILDDIR%\xenvbd.sys xenvbd\%BUILDDIR%\xencoinst.dll xenvbd\%BUILDDIR%\xenvbd.cat
 
 %DDK_PATH%\bin\selfsign\inf2cat /driver:xenscsi\%BUILDDIR% /os:%SIGN_OS%
 %DDK_PATH%\bin\selfsign\signtool sign /v /n %CERT_NAME% /t http://timestamp.verisign.com/scripts/timestamp.dll xenscsi\%BUILDDIR%\xenscsi.sys xenscsi\%BUILDDIR%\xenscsi.cat
