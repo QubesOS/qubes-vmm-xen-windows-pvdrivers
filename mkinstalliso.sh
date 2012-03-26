9c3929062b20c04d6590a0e1f30cf0024ed79ee6
Joanna Rutkowska <joanna@invisiblethingslab.com>
2012-03-26 15:16:11 +0200

    mkinstalliso.sh script for preparing an installation ISO

---
+++ mkinstalliso.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+SRCIMG=winpvsources.img
+ISOIMG=winpvdrivers.iso
+MNT=mnt
+ISODIR=iso/
+
+sudo kpartx -a $SRCIMG
+# Now, we're assuming that the part was mounted as /dev/mapper/loop0p1
+# I'm not sure how to check this?
+sudo mount /dev/mapper/loop0p1 $MNT
+
+cp $MNT/winpvdrivers/gplpv*.msi $ISODIR/
+if [ $? -ne 0 ]; then
+    echo "No installation files found! Have you built the drivers?"
+    sudo umount  $MNT 
+    sudo kpartx -d $SRCIMG
+    exit 1
+fi
+    
+sudo umount  $MNT
+sudo kpartx -d $SRCIMG
+genisoimage -o $ISOIMG -JR $ISODIR
+
