bf6c5e703e711362b8843eaa1d02f267226d3b4d
Marek Marczykowski <marmarek@invisiblethingslab.com>
2012-06-13 17:35:59 +0200

    build: obsolete creating iso directly, but leave building msi machinery
    Can be still useful for developers.

--- extractmsi.sh
+++ extractmsi.sh
@@ -1,27 +1,23 @@
 #!/bin/sh
 
 SRCIMG=winpvsources.img
-ISOIMG=pvdrivers-win7.iso
 MNT=mnt
-ISODIR=iso/
+OUTDIR=msi/
 
 OUTPUT=`sudo kpartx -a -v $SRCIMG`
 # sample output: add map loop0p1 (253:1): 0 2095104 linear /dev/loop0 2048
 DEV=/dev/mapper/`echo $OUTPUT | cut -f 3 -d ' '`
 sudo mount $DEV $MNT
 
-rm -f $ISODIR/*.msi
-cp $MNT/winpvdrivers/gplpv_Vista2008*.msi $ISODIR/
+mkdir -p $OUTDIR
+rm -f $OUTDIR/*.msi
+cp $MNT/winpvdrivers/gplpv_*.msi $OUTDIR/
 if [ $? -ne 0 ]; then
     echo "No installation files found! Have you built the drivers?"
     sudo umount  $MNT 
     sudo kpartx -d $SRCIMG
     exit 1
 fi
-    
+
 sudo umount  $MNT
 sudo kpartx -d $SRCIMG
-genisoimage -o $ISOIMG -m .gitignore -JR $ISODIR
-
-# Now, make also an RPM containg this ISO
-rpmbuild --target noarch --define "_rpmdir rpm/" -bb iso_rpm.spec
