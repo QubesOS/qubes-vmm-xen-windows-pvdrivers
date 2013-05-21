72958e54dbdd6f9fc9060c673bac892758d8aada
Marek Marczykowski <marmarek@invisiblethingslab.com>
2013-05-21 21:36:03 +0200

    xenstore: rename xs.h to xenstore.h to match official xen repo
    xs.h was obsoleted in xen 4.2 release, all new tools uses xenstore.h.

--- tools/xenstore/xs.c
+++ tools/xenstore/xs.c
@@ -19,7 +19,7 @@
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-#include "xs.h"
+#include "xenstore.h"
 #include "xs_lib.h"
 #include "list.h"
 #include <setupapi.h>
