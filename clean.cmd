8cffcc09c2f8c1204f5460a366fcd76ece1b0cc7
Rafał Wojdyła <omeg@invisiblethingslab.com>
2025-10-03 15:06:58 +0200

    Fix clean script
    Signed-off-by: Rafał Wojdyła <omeg@invisiblethingslab.com>

--- clean.cmd
+++ clean.cmd
@@ -3,9 +3,13 @@
 rd /s /q %~dp0\.artifacts
 rd /s /q %~dp0\vs2022\tmp
 rd /s /q %~dp0\vs2022\x64
-del /s /q /f %~dp0\sign.crt
-del /s /q /f %~dp0\include\qwt_version.h
+del /q /f %~dp0\sign.crt
+del /q /f %~dp0\include\qwt_version.h
 
+del /q %~dp0\xenbus\.build_number
+del /q %~dp0\xenbus\include\version.h
+del /q %~dp0\xenbus\vs2022\version\.revision
+del /q %~dp0\xenbus\vs2022\xenbus.inf
 rd /s /q %~dp0\xenbus\xenbus
 rd /s /q %~dp0\xenbus\vs2022\Windows10Debug
 rd /s /q %~dp0\xenbus\vs2022\Windows10Release
@@ -21,6 +25,12 @@ rd /s /q %~dp0\xenbus\vs2022\xenbus_monitor\x64
 rd /s /q %~dp0\xenbus\vs2022\xenfilt\Windows10Debug
 rd /s /q %~dp0\xenbus\vs2022\xenfilt\Windows10Release
 
+del /q %~dp0\xeniface\.build_number
+del /q %~dp0\xeniface\include\version.h
+del /q %~dp0\xeniface\src\xeniface\wmi.mof
+del /q %~dp0\xeniface\src\xeniface\wmi_generated.h
+del /q %~dp0\xeniface\vs2022\version\.revision
+del /q %~dp0\xeniface\vs2022\xeniface.inf
 rd /s /q %~dp0\xeniface\xeniface
 rd /s /q %~dp0\xeniface\vs2022\Windows10Debug
 rd /s /q %~dp0\xeniface\vs2022\Windows10Release
@@ -33,6 +43,10 @@ rd /s /q %~dp0\xeniface\vs2022\xeniface\Windows10Debug
 rd /s /q %~dp0\xeniface\vs2022\xeniface\Windows10Release
 rd /s /q %~dp0\xeniface\vs2022\xeniface\x64
 
+del /q %~dp0\xennet\.build_number
+del /q %~dp0\xennet\include\version.h
+del /q %~dp0\xennet\vs2022\version\.revision
+del /q %~dp0\xennet\vs2022\xennet.inf
 rd /s /q %~dp0\xennet\xennet
 rd /s /q %~dp0\xennet\vs2022\Windows10Debug
 rd /s /q %~dp0\xennet\vs2022\Windows10Release
@@ -43,6 +57,10 @@ rd /s /q %~dp0\xennet\vs2022\xennet\Windows10Debug
 rd /s /q %~dp0\xennet\vs2022\xennet\Windows10Release
 rd /s /q %~dp0\xennet\vs2022\xennet\x64
 
+del /q %~dp0\xenvbd\.build_number
+del /q %~dp0\xenvbd\include\version.h
+del /q %~dp0\xenvbd\vs2022\version\.revision
+del /q %~dp0\xenvbd\vs2022\xenvbd.inf
 rd /s /q %~dp0\xenvbd\xenvbd
 rd /s /q %~dp0\xenvbd\vs2022\Windows10Debug
 rd /s /q %~dp0\xenvbd\vs2022\Windows10Release
@@ -58,6 +76,10 @@ rd /s /q %~dp0\xenvbd\vs2022\xendisk\x64
 rd /s /q %~dp0\xenvbd\vs2022\xenvbd\Windows10Debug
 rd /s /q %~dp0\xenvbd\vs2022\xenvbd\Windows10Release
 
+del /q %~dp0\xenvif\.build_number
+del /q %~dp0\xenvif\include\version.h
+del /q %~dp0\xenvif\vs2022\version\.revision
+del /q %~dp0\xenvif\vs2022\xenvif.inf
 rd /s /q %~dp0\xenvif\xenvif
 rd /s /q %~dp0\xenvif\vs2022\Windows10Debug
 rd /s /q %~dp0\xenvif\vs2022\Windows10Release
